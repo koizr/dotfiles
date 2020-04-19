95a8c7992388b7526851c17e12265c69d36c1d9b
koizr <koizumi.ryo.dev@gmail.com>
2020-04-19 23:48:16 +0900

    add setup neovim

--- setup.sh
+++ setup.sh
@@ -3,5 +3,11 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew bundle
 
+# neovim
+pip3 install neovim
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+sh ./installer.sh ~/.cache/dein
+rm ./installer.sh
+
 # https://www.jenv.be/
 jenv add /Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home
