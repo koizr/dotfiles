10154b7a320a63aac1123f112fcc70bbed549d62
koizr <koizumi.ryo.dev@gmail.com>
2023-01-19 22:21:36 +0900

    add install script commands

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+set -eu
+
+# Homebrew
+# https://brew.sh/
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew install \
@@ -25,3 +29,11 @@ brew install --cask \
     visual-studio-code \
     jetbrains-toolbox \
     google-chrome firefox
+
+# zsh
+# https://github.com/zdharma-continuum/zinit#automatic
+bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
+
+# Vim
+# https://github.com/tani/vim-jetpack#installation
+curl -fLo ~/.local/share/nvim/site/pack/jetpack/opt/vim-jetpack/plugin/jetpack.vim --create-dirs https://raw.githubusercontent.com/tani/vim-jetpack/master/plugin/jetpack.vim
