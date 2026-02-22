d66914a730ffefa7ae29ad728a2e8620fefd2e99
koizr <koizumi.ryo.dev@gmail.com>
2026-02-23 00:01:35 +0900

    vim: show hidden files in explorer

---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,17 @@
+return {
+    {
+        "folke/snacks.nvim",
+        opts = {
+            picker = {
+                hidden = true,
+                ignored = true,
+                exclude = { ".git", "node_modules" },
+                sources = {
+                    explorer = {
+                        include = { "node_modules" },
+                    },
+                },
+            },
+        },
+    },
+}
