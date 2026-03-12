760a5782508b8186332f37d02c6570cdecf0ed23
koizr <koizumi.ryo.dev@gmail.com>
2026-03-12 14:46:35 +0900

    vim: include hidden files on finder

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -5,8 +5,12 @@ return {
       picker = {
         hidden = true,
         ignored = true,
-        exclude = { ".git", "node_modules" },
+        exclude = { ".git", "node_modules", ".DS_Store" },
         sources = {
+          files = {
+            hidden = true,
+            ignored = true,
+          },
           explorer = {
             include = { "node_modules" },
           },
