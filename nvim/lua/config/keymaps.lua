e72d5991c771a82916de2cfbcc95f3bad3c4f101
koizr <koizumi.ryo.dev@gmail.com>
2026-03-19 10:12:09 +0900

    vim: copy file path shortcut

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,21 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>yL", function()
+  local text = vim.fn.expand("%:.") .. ":" .. vim.fn.line(".")
+  vim.fn.setreg("+", text)
+  vim.notify("Copied: " .. text)
+end, { desc = "Copy file path:line to clipboard" })
+
+vim.keymap.set("v", "<leader>yL", function()
+  local start_line = vim.fn.line("v")
+  local end_line = vim.fn.line(".")
+  if start_line > end_line then
+    start_line, end_line = end_line, start_line
+  end
+
+  local text = vim.fn.expand("%:.") .. ":" .. start_line .. "-" .. end_line
+  vim.fn.setreg("+", text)
+  vim.notify("Copied: " .. text)
+end, { desc = "Copy file path:line-range to clipboard" })
