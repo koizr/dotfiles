3ce99d91ca90b0e7a6303ec460a774aefd677a15
koizr <koizumi.ryo.dev@gmail.com>
2026-03-24 09:16:06 +0900

    vim: add gitlinker

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,13 +2,13 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<leader>yL", function()
+vim.keymap.set("n", "<leader>yl", function()
   local text = vim.fn.expand("%:.") .. ":" .. vim.fn.line(".")
   vim.fn.setreg("+", text)
   vim.notify("Copied: " .. text)
 end, { desc = "Copy file path:line to clipboard" })
 
-vim.keymap.set("v", "<leader>yL", function()
+vim.keymap.set("v", "<leader>yl", function()
   local start_line = vim.fn.line("v")
   local end_line = vim.fn.line(".")
   if start_line > end_line then
---
+++ nvim/lua/plugins/gitliner.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "linrongbin16/gitlinker.nvim",
+    cmd = "GitLink",
+    opts = {},
+    keys = {
+      { "<leader>yL", "<cmd>GitLink<cr>", mode = { "n", "v" }, desc = "Copy git remote link" },
+    },
+  },
+}
