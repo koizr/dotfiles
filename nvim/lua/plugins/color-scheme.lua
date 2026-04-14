333d0c62e3ced070399fd4212102f9e6ee44137c
koizr <koizumi.ryo.dev@gmail.com>
2026-04-14 10:26:42 +0900

    vim: GitHub color scheme

--- nvim/lua/plugins/color-scheme.lua
+++ nvim/lua/plugins/color-scheme.lua
@@ -1,55 +1,16 @@
 return {
   {
-    "catppuccin/nvim",
+    "projekt0n/github-nvim-theme",
     lazy = false,
     priority = 1000,
-    name = "catppuccin",
+    name = "github-theme",
     opts = {
-      flavour = "macchiato",
-      lsp_styles = {
-        underlines = {
-          errors = { "undercurl" },
-          hints = { "undercurl" },
-          warnings = { "undercurl" },
-          information = { "undercurl" },
+      options = {
+        styles = {
+          comments = "italic",
+          keywords = "bold",
+          types = "italic",
         },
-      },
-      integrations = {
-        aerial = true,
-        alpha = true,
-        cmp = true,
-        dashboard = true,
-        flash = true,
-        fzf = true,
-        grug_far = true,
-        gitsigns = true,
-        headlines = true,
-        illuminate = true,
-        indent_blankline = { enabled = true },
-        leap = true,
-        lsp_trouble = true,
-        mason = true,
-        mini = true,
-        navic = { enabled = true, custom_bg = "lualine" },
-        neotest = true,
-        neotree = true,
-        noice = true,
-        notify = true,
-        snacks = true,
-        telescope = true,
-        treesitter_context = true,
-        which_key = true,
-      },
-    },
-    specs = {
-      {
-        "akinsho/bufferline.nvim",
-        optional = true,
-        opts = function(_, opts)
-          if (vim.g.colors_name or ""):find("catppuccin") then
-            opts.highlights = require("catppuccin.special.bufferline").get_theme()
-          end
-        end,
       },
     },
   },
