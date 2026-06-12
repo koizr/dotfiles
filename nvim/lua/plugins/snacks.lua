e562310e7dad5268f50c304130f0f2960db13274
koizr <koizumi.ryo.dev@gmail.com>
2026-06-12 15:09:07 +0900

    vim: include mise.local.toml in picker

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -4,7 +4,7 @@ return {
     opts = {
       picker = {
         hidden = true,
-        include = { ".env", ".env.local" },
+        include = { ".env", ".env.local", "mise.local.toml" },
         exclude = { ".git", "node_modules", ".DS_Store" },
         sources = {
           files = {
